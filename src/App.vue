42149afc6357690b6d53cf143486d4b72e4ad7e7
fah <fah@ciklum.com>
2021-10-14 17:35:45 +0500

    Emit Events from Child and handle it in parent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <h1>{{ title }}</h1>
   <div>
-    <input type="text" ref="name">
-    <br>
+    <input type="text" ref="name" />
+    <br />
     <button @click="handleClick">Click</button>
   </div>
-  <Modal header="Sign Up for Giveaways" text="Get the latest updates as soon as we give them away"/>
+  <Modal v-if="showModal" :header="header" :text="text" :theme="theme" @close="toggleModal" />
+  <button @click="toggleModal">Show modal</button>
 </template>
 
 <script>
-import Modal from "./components/Modal.vue"
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
@@ -17,14 +18,21 @@ export default {
   data() {
     return {
       title: "My first App",
+      header: "Sign Up for Giveaways",
+      text: "Get the latest updates as soon as we give them away nowwww",
+      theme: "sales",
+      showModal: false,
     };
   },
   methods: {
-    handleClick(){
+    handleClick() {
       this.$refs.name.classList.add("anotherclass");
-      this.$refs.name.focus()
-    }
-  }
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
 };
 </script>
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{sale : theme === 'sales'}">
       <h1>{{ header }}</h1>
       <p>{{ text }}</p>
     </div>
@@ -9,7 +9,12 @@
 
 <script>
 export default {
-  props: ['header','text']
+  props: ['header','text','theme'],
+  methods:{
+    closeModal(){
+      this.$emit('close');
+    }
+  },
 };
 </script>
 
@@ -36,4 +41,12 @@ export default {
 .modal p {
   font-style: normal;
 }
+  /* sale styles */
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
 </style>
